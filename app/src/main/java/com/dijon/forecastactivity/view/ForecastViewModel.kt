eb62197886f0f671758d271bb4b77d16894fad71
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-05 18:19:54 -0400

    Add ForecastViewModel with liveData

---
+++ app/src/main/java/com/dijon/forecastactivity/view/ForecastViewModel.kt
@@ -0,0 +1,25 @@
+package com.dijon.forecastactivity.view
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dijon.forecastactivity.api.WebApiAccess
+import com.dijon.forecastactivity.model.WeatherResult
+import kotlinx.coroutines.launch
+
+class ForecastViewModel() : ViewModel() {
+
+    private val _results = MutableLiveData<WeatherResult>()
+    val results: LiveData<WeatherResult> get() = _results
+
+    fun getForecast() = viewModelScope.launch {
+        val bodyResult = WebApiAccess.weatherApi.getForecastList(
+            "Manaus",
+            "7dddf551a056642a1ae589fdb97aa5ec",
+            "pt_br",
+            "metric"
+        ).body()
+        _results.postValue(bodyResult)
+    }
+}
