5002ef230c42b2aab70649ed6de74a572ab31708
dijoncavalcante <dijoncavalcante@gmail.com>
2021-03-05 18:10:53 -0400

    Add import retrofit2.Retrofit

---
+++ app/src/main/java/com/dijon/forecastactivity/api/WeatherApiClient.kt
@@ -0,0 +1,17 @@
+package com.dijon.forecastactivity.api
+
+import com.dijon.forecastactivity.model.WeatherResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//http://api.openweathermap.org/data/2.5/forecast?q=Manaus&appid=7dddf551a056642a1ae589fdb97aa5ec&lang=pt_br&units=metric
+interface WeatherApiClient {
+    @GET("forecast")
+    suspend fun getForecastList(
+        @Query("q") city: String,
+        @Query("appid") id: String,
+        @Query("lang") lang: String,
+        @Query("units") units: String
+    ): Response<WeatherResult>
+}
